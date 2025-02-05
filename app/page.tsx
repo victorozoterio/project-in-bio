e259ed783ff55ece33433f5e4adbd4ac2bd37323
Victor Ozoterio <victorozoterio@gmail.com>
2025-02-05 20:28:02 -0300

    feat: criando o header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "./components/landing-page/hero";
+import Header from "./components/landing-page/header";
 
 export default function Home() {
 	return (
 		<div className="max-w-7xl mx-auto">
+			<Header />
 			<Hero />
 		</div>
 	);
