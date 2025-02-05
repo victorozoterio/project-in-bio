2d29991e07578005dd4f19e4771eac901dcec404
Victor Ozoterio <victorozoterio@gmail.com>
2025-02-05 15:23:58 -0300

    feat: desenvolvendo hero section

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,10 +1,11 @@
+import UserCard from "../commons/user-card";
 import Button from "../ui/button";
 import TextInput from "../ui/text-input";
 
 export default function Hero() {
 	return (
-		<div className="flex border">
-			<div className="w-full flex flex-col gap-2 mt-[35vh] border">
+		<div className="flex h-screen">
+			<div className="w-full flex flex-col gap-2 mt-[35vh]">
 				<h1 className="text-5xl font-bold text-white leading-[64px]">
 					Seus projetos e redes sociais em um único link
 				</h1>
@@ -19,9 +20,9 @@ export default function Hero() {
 					<Button>Criar agora</Button>
 				</div>
 			</div>
-			<div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB, transparent_55%)]">
+			<div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
 				<div className="relative">
-					{/* <UserCard /> */}
+					<UserCard />
 					<div className="absolute -bottom-[7%] -right-[45%]">
 						{/* <TotalVisits /> */}
 					</div>
