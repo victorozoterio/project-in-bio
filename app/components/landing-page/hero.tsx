a1ef6d24e84c93b2d8be2b3a2d634293313b1a1e
Victor Ozoterio <victorozoterio@gmail.com>
2025-02-05 15:32:19 -0300

    feat: criando componente total de visitas

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,3 +1,4 @@
+import { TotalVisits } from "../commons/total-visits";
 import UserCard from "../commons/user-card";
 import Button from "../ui/button";
 import TextInput from "../ui/text-input";
@@ -24,7 +25,7 @@ export default function Hero() {
 				<div className="relative">
 					<UserCard />
 					<div className="absolute -bottom-[7%] -right-[45%]">
-						{/* <TotalVisits /> */}
+						<TotalVisits />
 					</div>
 					<div className="absolute top-[20%] -left-[45%] -z-10">
 						{/* <ProjectCard/> */}
