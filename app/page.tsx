911fafb5e2c534ff6ab82d6df596294976af87c6
Victor Ozoterio <victorozoterio@gmail.com>
2025-02-05 21:34:33 -0300

    feat: criando faq section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/landing-page/hero";
 import Header from "./components/landing-page/header";
 import VideoExplanation from "./components/landing-page/video-explanation";
 import Pricing from "./components/landing-page/pricing";
+import FAQ from "./components/landing-page/faq";
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 			<Hero />
 			<VideoExplanation />
 			<Pricing />
+			<FAQ />
 		</div>
 	);
 }
